f19e1ff44b0fe80dc15b26e994df3b18fc0a004f
Tomasz Homoncik <tjhomoncik@gmail.com>
2020-12-20 20:22:16 +0100

    part1 day 20

---
+++ day20.hs
@@ -0,0 +1,57 @@
+import Data.List
+import Data.List as List
+import Data.List.Split as Split
+import Data.Map as Map
+import Data.Set as Set
+
+tileId :: [String] -> Integer
+tileId = read . head . List.drop 1 . Split.splitOneOf ": " . head
+
+flipped :: (String, String, String, String) -> (String, String, String, String)
+flipped (left, right, top, bot) = (right, left, reverse top, reverse bot)
+
+rotations ::
+     (String, String, String, String) -> [(String, String, String, String)]
+rotations tile = rotations' tile ++ rotations' (flipped tile)
+
+rotations' ::
+     (String, String, String, String) -> [(String, String, String, String)]
+rotations' tile@(left, right, top, bot) =
+  [ tile -- normal
+  , (bot, top, reverse left, reverse right) -- 90 deg
+  , (reverse right, reverse left, reverse top, reverse bot) -- 180 deg
+  , (reverse top, reverse bot, right, left) -- -90 deg
+  ]
+
+edges :: [String] -> (String, String, String, String)
+edges tile = (left . List.drop 1 $ tile, right . List.drop 1 $ tile, top, bot)
+  where
+    left s = List.map (head) s
+    right s = List.map (List.last) s
+    top = head . List.drop 1 $ tile
+    bot = List.last tile
+
+tilesMatch :: [String] -> [String] -> Bool
+tilesMatch a b =
+  any
+    (== True)
+    [ True
+    | x@(al, ar, at, ab) <- rotations (edges a)
+    , y@(bl, br, bt, bb) <- rotations (edges b)
+    , ar == bl
+    ]
+
+corner :: [String] -> [[String]] -> Bool
+corner tile allTiles =
+  (== 3) .
+  List.length .
+  List.map snd .
+  List.filter ((== True) . fst) . List.map (\b -> (tilesMatch tile b, tileId b)) $
+  allTiles
+
+main :: IO ()
+main = do
+  str <- readFile "input.txt"
+  let input = str
+  let foo = List.map lines . Split.splitOn "\n\n" $ input
+  print $ product . List.map fst . List.filter snd . List.map (\x -> (tileId x, corner x foo)) $ foo
